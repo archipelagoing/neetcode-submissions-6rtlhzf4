d2838bbf2e61cc0c9f2549fb6c833e6ae5e6037e
Archisa Bhattacharya <65607180+archipelagoing@users.noreply.github.com>
2026-03-31 13:06:44 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        lowest = prices[0]
+        tempProfit = 0
+        for x in prices:
+            if x < lowest:
+                lowest = x
+            else:
+                if x-lowest > tempProfit:
+                    tempProfit = x-lowest
+                    
+        return tempProfit
+
+"""
+Greedy Algorithm
+1. loop thru the List
+2. make a variable that says the smallest price so far. 
+3. make a temporary profit that says the largest profit
+4. if the new variable at that iteration is greater than the 
+"""
+        
