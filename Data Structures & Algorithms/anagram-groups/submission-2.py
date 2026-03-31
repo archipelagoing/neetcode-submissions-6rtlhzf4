5f1a258946f7ca8fbadb0a057422d76a7dcda20a
Archisa Bhattacharya <65607180+archipelagoing@users.noreply.github.com>
2026-03-31 12:31:39 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # res = defaultdict(list)
+        # for s in strs:
+        #     sortedS = ''.join(sorted(s))
+        #     res[sortedS].append(s)
+        # return list(res.values())
+        # # time complexity is too long
+
+        res = defaultdict(list)  # mapping charCount to list of anagrams 
+
+        for s in strs:
+            count = [0] * 26 #a ... z
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
