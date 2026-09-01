ad4ea4e6fded65db2abd07fc00e31a2b16c776d2
Archisa Bhattacharya <65607180+archipelagoing@users.noreply.github.com>
2026-09-01 15:59:04 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # build an adjacency list saying what is the prerequisite for each course . 
+        preMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visiting = set()
+#----------------------
+        def dfs(crs):
+            if crs in visiting:
+                # cycle detected
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            # course can be taken; lets remove it from the visit set
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+#----------------------
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
